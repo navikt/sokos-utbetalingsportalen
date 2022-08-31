b9342ea900dfcc9ecc6452366b0e98b739f6e825
mudasar187 <mudasar_cheema@hotmail.com>
2022-08-31 13:04:09 +0200

    laster opp ferdig oppsett, sjekker om den rendrer i nais

--- src/urls.jsx
+++ src/urls.jsx
@@ -1,27 +1,27 @@
 import { getEnvironment } from "./api/environment";
 
 const OKONOMIPORTALEN_URL = {
-  local: "http://localhost:3000/minside",
-  development: "https://www.dev.intern.nav.no/okonomiportalen",
-  production: "https://www.intern.nav.no/okonomiportalen",
+  local: "http://localhost:3000/okonomiportalen",
+  development: "https://www.okonomiportalen.dev.intern.nav.no/okonomiportalen",
+  production: "https://www.okonomiportalen.intern.nav.no/okonomiportalen",
 };
 
 const SOKOS_OKONOMIPORTALEN_PROXY_URL = {
   local: "http://localhost:3000/sokos-okonomiportalen-proxy",
-  development: "https://www.dev.intern.nav.no/sokos-okonomiportalen-proxy",
-  production: "https://www.intern.nav.no/sokos-okonomiportalen-proxy",
+  development: "https://www.okonomiportalen.dev.intern.nav.no/sokos-okonomiportalen-proxy",
+  production: "https://www.okonomiportalen.intern.nav.no/sokos-okonomiportalen-proxy",
 };
 
 const BASE_URL = {
   local: "http://localhost:3000",
-  development: "https://www.dev.intern.nav.no",
-  production: "https://www.intern.nav.no",
+  development: "https://okonomiportalen.dev.intern.nav.no",
+  production: "https://www.okonomiportalen.nav.no",
 };
 
 const ATTESTASJON_URL = {
   local: "http://localhost:3000/sokos-attestasjon-frontend/bundle.js",
-  development: "https://veientilarbeid.dev.nav.no/esm/bundle.js",
-  production: "https://veientilarbeid.nav.no/esm/bundle.js",
+  development: "https://okonomiportalen.dev.intern.nav.no/sokos-attestasjon-frontend/bundle.js",
+  production: "https://okonomiportalen.intern.nav.no/sokos-attestasjon-frontend/bundle.js",
 };
 
 export const okonomiportalenUrl = OKONOMIPORTALEN_URL[getEnvironment()];
