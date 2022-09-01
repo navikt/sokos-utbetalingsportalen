af7ecb8a914e2df08302529a74cd9497bc18b25a
mudasar187 <mudasar_cheema@hotmail.com>
2022-09-01 09:50:47 +0200

    Tar bort www fra alle url for å se om problemet ligger her
    Co-authored-by: Jeevesh Mishra <jeevesh.mishra@nav.no>

--- src/urls.jsx
+++ src/urls.jsx
@@ -2,20 +2,20 @@ import { getEnvironment } from "./api/environment";
 
 const OKONOMIPORTALEN_URL = {
   local: "http://localhost:3000/okonomiportalen",
-  development: "https://www.okonomiportalen.dev.intern.nav.no/okonomiportalen",
-  production: "https://www.okonomiportalen.intern.nav.no/okonomiportalen",
+  development: "https://okonomiportalen.dev.intern.nav.no/okonomiportalen",
+  production: "https://okonomiportalen.intern.nav.no/okonomiportalen",
 };
 
 const SOKOS_OKONOMIPORTALEN_PROXY_URL = {
   local: "http://localhost:3000/sokos-okonomiportalen-proxy",
-  development: "https://www.okonomiportalen.dev.intern.nav.no/sokos-okonomiportalen-proxy",
-  production: "https://www.okonomiportalen.intern.nav.no/sokos-okonomiportalen-proxy",
+  development: "https://okonomiportalen.dev.intern.nav.no/sokos-okonomiportalen-proxy",
+  production: "https://okonomiportalen.intern.nav.no/sokos-okonomiportalen-proxy",
 };
 
 const BASE_URL = {
   local: "http://localhost:3000",
   development: "https://okonomiportalen.dev.intern.nav.no",
-  production: "https://www.okonomiportalen.nav.no",
+  production: "https://okonomiportalen.nav.no",
 };
 
 const ATTESTASJON_URL = {
