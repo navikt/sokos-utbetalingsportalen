3a7412e15c19b52123b6ad4001a0a6c67660d397
mudasar187 <mudasar_cheema@hotmail.com>
2022-09-02 13:15:30 +0200

    Testing
    Co-authored-by: Jeevesh Mishra <jeevesh.mishra@nav.no>

--- src/components/meny/Meny.jsx
+++ src/components/meny/Meny.jsx
@@ -1,38 +1,24 @@
 import { System } from "@navikt/ds-icons";
 import { Dropdown, Header } from "@navikt/ds-react-internal";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { Popover, Button } from "@navikt/ds-react";
 import useStore, { selectLanguage } from "../../store/store";
 
 const Meny = () => {
   const language = useStore(selectLanguage);
+  const buttonRef = useRef(null);
+  const [openState, setOpenState] = useState(false);
 
   return (
-    <Header>
-      <Header.Title as="h1">Økonomiportalen</Header.Title>
-      <Dropdown>
-        <Header.Button as={Dropdown.Toggle} className="ml-auto">
-          <System style={{ fontSize: "1.5rem" }} title="Systemer og oppslagsverk" />
-        </Header.Button>
-
-        <Dropdown.Menu>
-          <Dropdown.Menu.GroupedList>
-            <Dropdown.Menu.GroupedList.Heading>Systemer og oppslagsverk</Dropdown.Menu.GroupedList.Heading>
-            <Dropdown.Menu.GroupedList.Item as={Link} to={`/attestasjon`}>
-              Attestasjon
-            </Dropdown.Menu.GroupedList.Item>
-          </Dropdown.Menu.GroupedList>
-        </Dropdown.Menu>
-      </Dropdown>
-      <Dropdown>
-        <Header.UserButton as={Dropdown.Toggle} name="Ola Normann" description="Enhet: Skien" className="ml-auto" />
-        <Dropdown.Menu>
-          <Dropdown.Menu.List>
-            <Dropdown.Menu.List.Item>Logg ut</Dropdown.Menu.List.Item>
-          </Dropdown.Menu.List>
-        </Dropdown.Menu>
-      </Dropdown>
-    </Header>
+    <>
+      <Button ref={buttonRef} onClick={() => setOpenState(true)}>
+        Åpne popover
+      </Button>
+      <Popover open={openState} onClose={() => setOpenState(false)} anchorEl={buttonRef.current} placement="top">
+        <Popover.Content>Innhold her!</Popover.Content>
+      </Popover>
+    </>
   );
 };
 
