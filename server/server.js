f8e0a7a6cb9b6b3d75895983f2521dd3d1e953a8
mudasar187 <mudasar_cheema@hotmail.com>
2022-09-05 12:59:07 +0200

    bruke expressicGzip for å serve frontend

--- server/server.js
+++ server/server.js
@@ -4,10 +4,18 @@ const path = require("path");
 const basePath = "/okonomiportalen";
 const buildPath = path.resolve(__dirname, "../dist");
 const server = express();
+const expressStaticGzip = require("express-static-gzip");
 
 server.disable("x-powered-by");
 
-server.use(basePath, express.static(buildPath, { index: false }));
+server.use(
+  basePath,
+  expressStaticGzip(buildPath, {
+    index: false,
+    enableBrotli: true,
+    orderPreference: ["br"],
+  })
+);
 
 server.get(`${basePath}/internal/isAlive`, async (req, res) => {
   res.sendStatus(200);
@@ -20,5 +28,4 @@ server.get(`${basePath}/internal/isReady`, async (req, res) => {
 // Match everything except internal og static
 server.use(/^(?!.*\/(internal|static)\/).*$/, (req, res) => res.sendFile(`${buildPath}/index.html`));
 
-const port = 7100;
-server.listen(port, () => console.info(`Listening on port ${port}`));
+server.listen(7100, () => console.log("Server listening on port 7100"));
