ab1a1b74e0c36c80a831bf9ef0763a32af1ade75
mudasar187 <mudasar_cheema@hotmail.com>
2022-09-16 14:37:05 +0200

    teste å se om heading rendrer og om det påvirker css

--- src/components/hjem/Hjem.jsx
+++ src/components/hjem/Hjem.jsx
@@ -1,14 +1,12 @@
 import React from "react";
+import { Heading } from "@navikt/ds-react";
 import HjemCSS from "./Hjem.module.css";
 
 const Hjem = () => {
   return (
-    <div className={HjemCSS.hjem}>
-      <h1>
-        Landingside etter login og landingside når "hjem" knappen trykkes øverst i venstre hjørne på teksten
-        "Økonomiportalen"
-      </h1>
-    </div>
+    <Heading level="1" size="medium" className="text-center">
+      The red fox jumps over the lazy brown dog.
+    </Heading>
   );
 };
 
