dfdc5c4198efce424bdd939b3c5e6c2bce225e9e
mudasar187 <mudasar_cheema@hotmail.com>
2023-01-25 14:53:20 +0100

    fikse husky

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:jsx-a11y/recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "jsx-a11y"],
+  ignorePatterns: [".eslintrc.*", "**/dist/**/*", "vite.config.js", "/server/*"],
+  rules: {
+    //ref https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
---
+++ .eslintrc.pre-commit.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [".eslintrc.js"],
+  rules: {
+    //TODO: enable rules
+    "react/prop-types": "warn",
+    "no-unused-vars": "warn",
+    "no-undef": "warn",
+    "react/display-name": "warn",
+  },
+};
