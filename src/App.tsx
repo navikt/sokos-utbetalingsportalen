b94be75cbea0a51cf41d10cc312dc58ad36a0b09
mudasar187 <mudasar_cheema@hotmail.com>
2023-03-09 10:37:28 +0100

    fjenrer test knappene for errorboundry

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,11 @@ import Hjem from "./components/hjem/Hjem";
 import Layout from "./components/layout/Layout";
 import Meny from "./components/meny/Meny";
 import Attestasjon from "./micro-frontend/Attestasjon";
-import useIsErrorStore, { selectSetIsError } from "./store/store";
-import classes from "./App.module.css";
 
 const App = () => {
-  const setIsError = useIsErrorStore(selectSetIsError);
-
   return (
     <Router>
       <Meny />
-      <button className={classes.button_bra} onClick={() => setIsError(false)}>
-        REDDA
-      </button>
-      <button className={classes.button_dumt} onClick={() => setIsError(true)}>
-        FEIL
-      </button>
       <Layout>
         <Routes>
           <Route path="/" element={<Hjem />} />
