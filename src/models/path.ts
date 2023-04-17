70c42818e2fcba93476f82feeed0597f5ac2feb3
mudasar187 <mudasar_cheema@hotmail.com>
2023-04-17 09:15:44 +0200

    oppdatere url på poc

--- src/models/path.ts
+++ src/models/path.ts
@@ -1,4 +1,4 @@
 export enum Path {
   SOKOS_MIKROFRONTEND_TEMPLATE = "/sokos-mikrofrontend-template",
-  UTBETALINGER_FRONTEND_POC = "/utbetaling-frontend-poc",
+  UTBETALINGER_FRONTEND_POC = "/utbetalinger-frontend-poc",
 }
