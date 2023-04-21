09a482be7917f9bca4356d88ddc6d9056b7faa4a
mudasar187 <mudasar_cheema@hotmail.com>
2023-04-21 13:44:56 +0200

    teste logout

--- server/server.js
+++ server/server.js
@@ -21,10 +21,13 @@ server.use(
   })
 );
 
-server.get("/logout", (req, res) => {
-  console.log("AUTH HEADER:: ", req.headers.authorization);
-  req.headers.authorization = "";
-  res.redirect("/");
+server.get("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      next(err);
+    }
+  });
+  return res.redirect("/oauth2/logout/local?post_logout_redirect_uri=okonomiportalen.intern.nav.no");
 });
 
 server.get(`${basePath}/internal/isAlive`, async (_req, res) => {
