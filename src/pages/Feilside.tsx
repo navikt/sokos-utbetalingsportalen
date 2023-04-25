87c516e82a940f36f467bf5d6726de8eead34edb
Lars Hartvigsen <lars.hartvigsen@nav.no>
2023-04-25 16:19:24 +0200

    Går tilbake til riktig sted

--- src/pages/Feilside.tsx
+++ src/pages/Feilside.tsx
@@ -1,26 +1,23 @@
 import classes from "./Feilside.module.css";
 import { ChatExclamationmarkIcon } from "@navikt/aksel-icons";
-import { Button } from "@navikt/ds-react";
-import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 
 export type FeilsideProps = {
   tittel: string;
   melding: string;
   knapp?: any;
 };
-const Feilside = ({ tittel, melding, knapp }: FeilsideProps) => {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate(-1);
-  };
-  return (
-    <div className={classes["error-container"]}>
-      <ChatExclamationmarkIcon title="a11y-title" fontSize="6rem" />
-      <h2 className={classes["error-heading"]}>{tittel}</h2>
-      <div className={classes["error-message"]}>{melding}</div>
-      {knapp && <Button onClick={handleClick}>Gå tilbake</Button>}
-    </div>
-  );
-};
+const Feilside = ({ tittel, melding }: FeilsideProps) => (
+  <div className={classes["error-container"]}>
+    <ChatExclamationmarkIcon title="a11y-title" fontSize="6rem" />
+    <h2 className={classes["error-heading"]}>{tittel}</h2>
+    <div className={classes["error-message"]}>{melding}</div>
+    <p>
+      <Link to={".."} relative={"path"}>
+        Gå tilbake
+      </Link>
+    </p>
+  </div>
+);
 
 export default Feilside;
