a22541c8e7c9f890b2fb107fc46eb447cb4afd7c
mudasar187 <mudasar_cheema@hotmail.com>
2023-04-26 12:15:03 +0200

    rename til server.ts for package.json for å kunne starte opp server i main

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,8 @@
-import * as Sentry from "@sentry/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { getEnvironment } from "./api/environment";
 import App from "./App.js";
 import "./main.css";
 import Authentication from "./components/authentication/Authentication";
-
-if (getEnvironment() === "development") {
-  Sentry.init({
-    dsn: "https://22607cbddbde496281d3954c8340e6cb@sentry.gc.nav.no/160",
-    integrations: [new Sentry.BrowserTracing()],
-    tracesSampleRate: 0.1,
-  });
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
