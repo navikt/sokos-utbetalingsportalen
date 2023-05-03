effe9da3fe499182f6f7236561957a6bf082aee1
mudasar187 <mudasar_cheema@hotmail.com>
2023-05-03 13:31:30 +0200

    legge flere env i naiserator, samles et sted

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,7 +2,32 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-interface EnvConfig {
+// Interface to load env variables
+// Note these variables can possibly be undefined
+// as someone could skip these varibales or not setup a .env file at all
+interface ENV {
+  // Server env
+  SERVER_BASE_PATH: string | undefined;
+  SERVER_PORT: string | undefined;
+
+  // AzureAD env
+  AZURE_APP_WELL_KNOWN_URL: string | undefined;
+  AZURE_APP_CLIENT_ID: string | undefined;
+  AZURE_APP_CLIENT_SECRET: string | undefined;
+  AZURE_OPENID_CONFIG_JWKS_URI: string | undefined;
+  AZURE_OPENID_CONFIG_TOKEN_ENDPOINT: string | undefined;
+
+  // NAIS env
+  NAIS_CLUSTER_NAME: string | undefined;
+
+  // REST urls env
+  SOKOS_MIKROFRONTEND_API_URL: string | undefined;
+}
+
+interface Config {
+  SERVER_BASE_PATH: string;
+  SERVER_PORT: string;
+
   AZURE_APP_WELL_KNOWN_URL: string;
   AZURE_APP_CLIENT_ID: string;
   AZURE_APP_CLIENT_SECRET: string;
@@ -14,31 +39,35 @@ interface EnvConfig {
   SOKOS_MIKROFRONTEND_API_URL: string;
 }
 
-const getConfig = (): EnvConfig => {
+// Loading process.env as ENV interface
+
+const getConfig = (): ENV => {
   return {
-    AZURE_APP_WELL_KNOWN_URL: process.env.AZURE_APP_WELL_KNOWN_URL || "",
-    AZURE_APP_CLIENT_ID: process.env.AZURE_APP_CLIENT_ID || "",
-    AZURE_APP_CLIENT_SECRET: process.env.AZURE_APP_CLIENT_SECRET || "",
-    AZURE_OPENID_CONFIG_JWKS_URI: process.env.AZURE_OPENID_CONFIG_JWKS_URI || "",
-    AZURE_OPENID_CONFIG_TOKEN_ENDPOINT: process.env.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT || "",
-    NAIS_CLUSTER_NAME: process.env.NAIS_CLUSTER_NAME || "",
-    SOKOS_MIKROFRONTEND_API_URL: process.env.SOKOS_MIKROFRONTEND_API_URL || "",
+    SERVER_BASE_PATH: process.env.SERVER_BASE_PATH,
+    SERVER_PORT: process.env.SERVER_PORT,
+    AZURE_APP_WELL_KNOWN_URL: process.env.AZURE_APP_WELL_KNOWN_URL,
+    AZURE_APP_CLIENT_ID: process.env.AZURE_APP_CLIENT_ID,
+    AZURE_APP_CLIENT_SECRET: process.env.AZURE_APP_CLIENT_SECRET,
+    AZURE_OPENID_CONFIG_JWKS_URI: process.env.AZURE_OPENID_CONFIG_JWKS_URI,
+    AZURE_OPENID_CONFIG_TOKEN_ENDPOINT: process.env.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT,
+    NAIS_CLUSTER_NAME: process.env.NAIS_CLUSTER_NAME,
+    SOKOS_MIKROFRONTEND_API_URL: process.env.SOKOS_MIKROFRONTEND_API_URL,
   };
 };
 
 // Throwing an Error if any field was undefined we don't
 // want our app to run and ensure
 // that these fields are accessible. If all is good return
-// it as EnvConfig which just removes the undefined from our type
+// it as Config which just removes the undefined from our type
 // definition.
 
-const getSanitzedConfig = (config: EnvConfig): EnvConfig => {
+const getSanitzedConfig = (config: ENV): Config => {
   for (const [key, value] of Object.entries(config)) {
     if (value === undefined) {
-      throw new Error(`Missing key ${key}`);
+      throw new Error(`Kunne ikke starte server, mangler ${key} env`);
     }
   }
-  return config as EnvConfig;
+  return config as Config;
 };
 
 const config = getConfig();
--- server/src/server.ts
+++ server/src/server.ts
@@ -10,8 +10,7 @@ import Configuration from "./config";
 
 export const server: Express = express();
 
-const basePath = "/okonomiportalen";
-const buildPath = path.resolve(__dirname, "../dist");
+const BUILD_PATH = path.resolve(__dirname, "../dist");
 
 const scopes = {
   mikrofrontendApi: `api://${Configuration.NAIS_CLUSTER_NAME}.okonomi.sokos-mikrofrontend-api/.default`,
@@ -23,8 +22,8 @@ const startServer = () => {
   server.disable("x-powered-by");
 
   server.use(
-    basePath,
-    expressStaticGzip(buildPath, {
+    Configuration.SERVER_BASE_PATH,
+    expressStaticGzip(BUILD_PATH, {
       index: false,
       enableBrotli: true,
       orderPreference: ["br"],
@@ -35,21 +34,22 @@ const startServer = () => {
     })
   );
 
-  server.get([`${basePath}/internal/isAlive`, `${basePath}/internal/isReady`], (_req: Request, res: Response) =>
-    res.sendStatus(200)
+  server.get(
+    [`${Configuration.SERVER_BASE_PATH}/internal/isAlive`, `${Configuration.SERVER_BASE_PATH}/internal/isReady`],
+    (_req: Request, res: Response) => res.sendStatus(200)
   );
 
   server.get("/brukerident", respondUnauthorizedIfNotLoggedIn, fetchUserId);
 
   proxyWithOboToken("/mikrofrontend-api", Configuration.SOKOS_MIKROFRONTEND_API_URL, scopes.mikrofrontendApi);
 
-  server.use(`/assets`, express.static(`${buildPath}/assets`));
+  server.use(`/assets`, express.static(`${BUILD_PATH}/assets`));
 
   server.get(["/", "/*"], redirectIfUnauthorized, (_req: Request, res: Response) =>
-    res.sendFile(`${buildPath}/index.html`)
+    res.sendFile(`${BUILD_PATH}/index.html`)
   );
 
-  server.listen(8080, () => console.log("Server listening on port 8080"));
+  server.listen(Configuration.SERVER_PORT, () => console.log(`Server listening on port ${Configuration.SERVER_PORT}`));
 };
 
 initializeAzureAd()
