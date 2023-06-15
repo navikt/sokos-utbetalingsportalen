31931f2077587fff38eea9a7e2fb7f12172ccab7
mudasar187 <mudasar_cheema@hotmail.com>
2023-06-15 13:01:20 +0200

    henter gjelderId fra state

--- src/micro-frontend/SokosMikrofrontendTemplate.tsx
+++ src/micro-frontend/SokosMikrofrontendTemplate.tsx
@@ -3,16 +3,18 @@ import { ErrorBoundary } from "react-error-boundary";
 import FeilMelding from "../components/feilmelding/Feilmelding.js";
 import ContentLoader from "../components/loader/ContentLoader.js";
 import { sokosMikrofrontendTemplateUrl } from "../urls.js";
+import useStore, { selectGjelderID } from "../store/store";
 
 const SokosMikrofrontendTemplateBundle: LazyExoticComponent<
   ({ gjelderId }: { gjelderId: string }) => React.ReactElement
 > = React.lazy(() => import(/* @vite-ignore */ sokosMikrofrontendTemplateUrl));
 
 const SokosMikrofrontendTemplate = () => {
+  const gjelderId = useStore(selectGjelderID);
   return (
     <React.Suspense fallback={<ContentLoader />}>
       <ErrorBoundary fallbackRender={() => <FeilMelding />}>
-        <SokosMikrofrontendTemplateBundle gjelderId={"1234"} />
+        <SokosMikrofrontendTemplateBundle gjelderId={gjelderId} />
       </ErrorBoundary>
     </React.Suspense>
   );
