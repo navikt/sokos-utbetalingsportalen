15d3f9d9d5bd5eba310ec4e76601b95b18f731fa
Maximilian Øystå Lloyd <maximilian.oysta.lloyd@nav.no>
2023-07-07 15:31:34 +0200

    Added dictionary type to read dictionaries

--- src/authentication/azureAdGroups.tsx
+++ src/authentication/azureAdGroups.tsx
@@ -4,19 +4,19 @@ const AD_GRUPPE_SOKOS_MF_MIKROFRONTEND_READ = {
   local: "1b717a23-d376-471c-9fc6-356299fadc2b",
   development: "1b717a23-d376-471c-9fc6-356299fadc2b",
   production: "",
-} as const;
+} as const satisfies Dictionary;
 
-const AD_GRUPPE_SOKOS_MF_UTBETALINGER_READ: Record<string, string> = {
+const AD_GRUPPE_SOKOS_MF_UTBETALINGER_READ = {
   local: "b01fb216-fcb3-4ede-b7da-71fffe859763",
   development: "b01fb216-fcb3-4ede-b7da-71fffe859763",
   production: "",
-};
+} as const satisfies Dictionary;
 
-const AD_GRUPPE_SOKOS_MF_SKATTEKORT_READ: Record<string, string> = {
+const AD_GRUPPE_SOKOS_MF_SKATTEKORT_READ = {
   local: "a13b4176-e328-4e1c-b181-ff676a7146b1",
   development: "a13b4176-e328-4e1c-b181-ff676a7146b1",
   production: "",
-};
+} as const satisfies Dictionary;
 
 type Dictionary = Record<string, string>;
 
