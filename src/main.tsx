7b3426d7a16eb21fe5814685b0312ccad63d5e72
mudasar187 <mudasar_cheema@hotmail.com>
2023-08-04 10:40:44 +0200

    Legger til grafana faro for metrics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
+import { initializeFaro } from "@grafana/faro-web-sdk";
 import "./main.css";
+import { getEnvironment } from "./utils/environment.js";
+
+type GrafanaFaroUrl = "https://telemetry.nav.no/collect" | "https://telemetry.ekstern.dev.nav.no/collect";
+
+function getMetricsUrl(): GrafanaFaroUrl {
+  return getEnvironment() === "production"
+    ? "https://telemetry.nav.no/collect"
+    : "https://telemetry.ekstern.dev.nav.no/collect";
+}
+
+initializeFaro({
+  url: getMetricsUrl(),
+  app: {
+    name: "sokos-op-fasade",
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
