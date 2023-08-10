566d7f2168dca8b75cf837a241d8d9cbcef40be3
mudasar187 <mudasar_cheema@hotmail.com>
2023-08-10 09:21:20 +0200

    refaktorerer OBO mekanismen ved å bruke funksjoner gitt av open-id

--- server/src/onBehalfOfToken.ts
+++ server/src/onBehalfOfToken.ts
@@ -1,11 +1,13 @@
 import { logger } from "./logger";
 import Config from "./config";
 import { z } from "zod";
+import { Client, errors, Issuer, TokenSet } from "openid-client";
+
+let _client: Client;
 
 const OboTokenSchema = z.object({
   access_token: z.string(),
   expires_in: z.number(),
-  ext_expires_in: z.number(),
   token_type: z.string(),
 });
 
@@ -18,19 +20,23 @@ const tokenCache: Record<Scope, Record<AccessToken, CachedOboToken>> = {};
 
 export async function getOnBehalfOfToken(accessToken: string, scope: string) {
   const cachedOboToken = tokenCache[scope]?.[accessToken];
+  console.log("printer ut cachedOboToken", cachedOboToken);
 
   if (cachedOboToken && oboTokenIsValid(cachedOboToken)) {
+    console.log("er inne i if");
     return cachedOboToken.token;
   } else {
+    console.log("er inne i else");
     const newOboToken = await fetchNewOnBehalfOfToken(accessToken, scope);
-    const expires = Date.now() + newOboToken.expires_in * 1000;
+    const expires = Date.now() + (newOboToken.expires_in ?? 0) * 1000;
 
     if (!tokenCache[scope]) {
+      console.log("er inne i tokenCache[scope]");
       tokenCache[scope] = {};
     }
 
     tokenCache[scope][accessToken] = {
-      token: newOboToken,
+      token: OboTokenSchema.parse(newOboToken),
       expires,
     };
 
@@ -38,44 +44,54 @@ export async function getOnBehalfOfToken(accessToken: string, scope: string) {
   }
 }
 
+async function client() {
+  const _jwk = Config.AZURE_OPENID_CONFIG_JWKS_URI;
+  const _issuer = await Issuer.discover(Config.AZURE_APP_WELL_KNOWN_URL);
+  _client = new _issuer.Client(
+    {
+      client_id: Config.AZURE_APP_CLIENT_ID,
+      client_secret: Config.AZURE_APP_CLIENT_SECRET,
+      token_endpoint_auth_method: "client_secret_post",
+    },
+    { keys: [JSON.parse(_jwk)] },
+  );
+  return _client;
+}
+
 async function fetchNewOnBehalfOfToken(accessToken: string, scope: string) {
-  const formData = {
+  const _client = await client();
+
+  const grantBody = {
     grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-    scope,
-    client_id: Config.AZURE_APP_CLIENT_ID,
-    client_secret: Config.AZURE_APP_CLIENT_SECRET,
     assertion: accessToken,
     requested_token_use: "on_behalf_of",
+    scope,
   };
 
   const url = Config.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT;
 
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      body: new URLSearchParams(formData),
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
+  return await _client
+    .grant(grantBody)
+    .then((tokenSet: TokenSet) => {
+      return tokenSet;
+    })
+    .catch((err) => {
+      switch (err.constructor) {
+        case errors.OPError:
+        case errors.RPError:
+          logger.error(`Failed to retrieve on behalf of token for scope "${scope}", got error:`, {
+            openIdClientError: err,
+            httpStatusFromAzure: err.response.statusCode + " " + err.response.statusMessage,
+            azureBody: err.response.azureBody,
+          });
+          break;
+      }
+      logger.error("Unknown error from openid-client", { error: err });
+      throw err;
     });
-
-    const body = await response.json();
-
-    if (response.ok) {
-      return OboTokenSchema.parse(body);
-    } else {
-      logger.error(
-        `Failed to retrieve on behalf of token for scope "${scope}", got status ${response.status} (${response.statusText}) reason:`,
-        body,
-      );
-
-      throw response;
-    }
-  } catch (e) {
-    throw e;
-  }
 }
 
 function oboTokenIsValid(token: CachedOboToken) {
+  console.log("sjekker om obo er valid");
   return token.expires >= Date.now() - 5000;
 }
