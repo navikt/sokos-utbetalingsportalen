e57358d39dbe6cb4ca47c491b92103099b2cc11a
mudasar187 <mudasar_cheema@hotmail.com>
2023-08-10 15:02:44 +0200

    legge til at data kan sendes ut fra fasaden til telemetry server til nav

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,6 +18,7 @@ server.use(
   helmet.contentSecurityPolicy({
     directives: {
       "script-src": ["'self'", "https://www.nav.no"],
+      "connect-src": ["'self'", "https://telemetry.ekstern.dev.nav.no"],
     },
   }),
 );
