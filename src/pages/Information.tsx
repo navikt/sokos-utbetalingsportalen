3b194c21c30555642a46a4049f8d029e337218cb
mudasar187 <mudasar_cheema@hotmail.com>
2023-09-15 13:25:03 +0200

    sletter lorem ipsum teksten

--- src/pages/Information.tsx
+++ src/pages/Information.tsx
@@ -7,19 +7,6 @@ const Information = () => {
       <Heading level="1" size="medium">
         Informasjon
       </Heading>
-      <div className={styles.informasjon__bodyLong}>
-        <BodyLong>Her skal jeg komme informasjonside for saksbehandlere</BodyLong>
-        <BodyLong>
-          Amet dolore non tempor incididunt dolor est enim aute commodo cillum quis. Ex esse veniam ipsum quis. Pariatur
-          duis do qui exercitation ut laboris sit veniam nostrud nulla esse. In aute sint enim reprehenderit ut
-          voluptate do id. Laborum irure qui officia aute ipsum. Exercitation dolor sunt deserunt non anim.
-        </BodyLong>
-        <BodyLong>
-          Amet dolore non tempor incididunt dolor est enim aute commodo cillum quis. Ex esse veniam ipsum quis. Pariatur
-          duis do qui exercitation ut laboris sit veniam nostrud nulla esse. In aute sint enim reprehenderit ut
-          voluptate do id. Laborum irure qui officia aute ipsum. Exercitation dolor sunt deserunt non anim.
-        </BodyLong>
-      </div>
     </>
   );
 };
