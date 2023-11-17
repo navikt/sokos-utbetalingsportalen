6efc156910918aa9646b6eca70fed8a477c15864
Lars Hartvigsen <lars.hartvigsen@nav.no>
2023-11-17 11:47:46 +0100

    hva med denne gruppen

--- src/auth/azureAdGroups.ts
+++ src/auth/azureAdGroups.ts
@@ -10,7 +10,7 @@ const AD_GRUPPE_SOKOS_MF_MIKROFRONTEND_READ = {
 
 const AD_GRUPPE_SOKOS_MF_POSTERINGSOK_READ = {
   local: "b01fb216-fcb3-4ede-b7da-71fffe859763",
-  development: "301f8689-c70d-4135-820a-4d031b6c5ff7",
+  development: "b01fb216-fcb3-4ede-b7da-71fffe859763",
   production: "25723642-e1da-4d15-87ce-8c4d2b87e33a",
 } as const satisfies Dictionary;
 
