274cf665a2e3138638b07a717de616027cff6e53
jonasnico <jonas.nicolaysen@nav.no>
2024-03-08 16:55:08 +0100

    lagt til cards for appene

--- src/pages/Information.tsx
+++ src/pages/Information.tsx
@@ -1,8 +1,10 @@
-import { BodyLong, BodyShort, GuidePanel, Heading, Link, List } from "@navikt/ds-react";
+import { BodyLong, BodyShort, Box, GuidePanel, Heading, Link, List } from "@navikt/ds-react";
 import { useLoaderData } from "react-router-dom";
 import { UserData } from "../models/userData";
 import pengesekk from "../../assets/images/pengesekk.svg";
 import styles from "./Information.module.css";
+import { ChevronRightIcon } from "@navikt/aksel-icons";
+import { ROUTE_PATH } from "../models/routePath";
 
 const Information = () => {
   const userInfo = useLoaderData() as UserData;
@@ -15,34 +17,73 @@ const Information = () => {
         </Heading>
       </div>
       <div className={styles.container}>
-        <GuidePanel poster illustration={<img src={pengesekk} alt="pengesekk" />}>
-          <Heading level="2" size="small" spacing>
-            Informasjon om utbetalingsportalen
-          </Heading>
-          <BodyLong spacing>
-            Utbetalingsportalen er en ny platform som på sikt skal overta funksjoner fra Økonomiportalen og Abetal i en
-            ny og forbedret versjon.
-          </BodyLong>
-          <List as="ul" size="small">
-            <List.Item>
-              <BodyShort>
-                <Link href="https://navno.sharepoint.com/" target="_blank">
-                  Les mer på Navet
-                </Link>
-              </BodyShort>
-            </List.Item>
-            <List.Item>
-              <BodyShort>
-                <Link
-                  href="https://navno.sharepoint.com/sites/POutbetaling/_layouts/15/Doc.aspx?sourcedoc=%7B90953D22-FE5D-4818-A341-2FC88D4E2705%7D&file=Tilgangsbestilling%20i%20ny%20utbetalingsportal.docx&action=default&mobileredirect=true&DefaultItemOpen=1"
-                  target="_blank"
-                >
-                  Tilgangsbestilling
-                </Link>
-              </BodyShort>
-            </List.Item>
-          </List>
-        </GuidePanel>
+        <div className={styles.guidepanel}>
+          <GuidePanel poster illustration={<img src={pengesekk} alt="pengesekk" />}>
+            <Heading level="2" size="small" spacing>
+              Informasjon om utbetalingsportalen
+            </Heading>
+            <BodyLong spacing>
+              Utbetalingsportalen er en ny platform som på sikt skal overta funksjoner fra Økonomiportalen og Abetal i
+              en ny og forbedret versjon.
+            </BodyLong>
+            <List as="ul" size="small">
+              <List.Item>
+                <BodyShort>
+                  <Link href="https://navno.sharepoint.com/" target="_blank">
+                    Les mer på Navet
+                  </Link>
+                </BodyShort>
+              </List.Item>
+              <List.Item>
+                <BodyShort>
+                  <Link
+                    href="https://navno.sharepoint.com/sites/POutbetaling/_layouts/15/Doc.aspx?sourcedoc=%7B90953D22-FE5D-4818-A341-2FC88D4E2705%7D&file=Tilgangsbestilling%20i%20ny%20utbetalingsportal.docx&action=default&mobileredirect=true&DefaultItemOpen=1"
+                    target="_blank"
+                  >
+                    Tilgangsbestilling
+                  </Link>
+                </BodyShort>
+              </List.Item>
+            </List>
+          </GuidePanel>
+        </div>
+        <Heading level="3" size="medium" spacing>
+          Apper
+        </Heading>
+        <div className={styles.apper}>
+          <Link variant="neutral" href="#" underline={false}>
+            <Box background="surface-alt-2-subtle" padding="6" shadow="medium" borderRadius="xlarge">
+              <div className={styles.apperContent}>
+                <BodyShort weight="semibold">Oppdragsinfo</BodyShort>
+                <ChevronRightIcon />
+              </div>
+            </Box>
+          </Link>
+          <Link variant="neutral" href="#" underline={false}>
+            <Box background="surface-alt-2-subtle" padding="6" shadow="medium" borderRadius="xlarge">
+              <div className={styles.apperContent}>
+                <BodyShort weight="semibold">Oppslag i Reskontro Stønad</BodyShort>
+                <ChevronRightIcon />
+              </div>
+            </Box>
+          </Link>
+          <Link variant="neutral" href="#" underline={false}>
+            <Box background="surface-alt-2-subtle" padding="6" shadow="medium" borderRadius="xlarge">
+              <div className={styles.apperContent}>
+                <BodyShort weight="semibold">Kontoregister person kontosøk</BodyShort>
+                <ChevronRightIcon />
+              </div>
+            </Box>
+          </Link>
+          <Link variant="neutral" href="#" underline={false}>
+            <Box background="surface-alt-2-subtle" padding="6" shadow="medium" borderRadius="xlarge">
+              <div className={styles.apperContent}>
+                <BodyShort weight="semibold">Skattekort</BodyShort>
+                <ChevronRightIcon />
+              </div>
+            </Box>
+          </Link>
+        </div>
       </div>
     </>
   );
