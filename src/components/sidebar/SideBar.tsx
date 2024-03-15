a0b31b1fa0badb2692106ae52001cb8deef11f73
jonasnico <jonas.nicolaysen@nav.no>
2024-03-15 13:43:15 +0100

    gjort om litt på struktur og bruker transition på width

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -22,74 +22,73 @@ const SideBar = () => {
 
   return (
     <>
-      <div
-        className={`transition-transform duration-500 bg-neutral-800 h-screen overflow-hidden top-12 left-0 flex-col ${
-          showSideBar ? "translate-x-0 w-64 min-w-64" : "-translate-x-full w-0 min-w-0"
-        }`}
-      >
-        <div className="p-3 flex justify-end text-white">
-          <Button
-            className="cursor-pointer flex flex-row items-center bg-neutral-800"
-            onClick={() => setShowSideBar(!showSideBar)}
-            icon={<XMarkIcon />}
-            iconPosition="right"
-            variant="primary-neutral"
-          >
-            Lukk
-          </Button>
+      {showSideBar ? (
+        <div
+          className={`${styles.sidebar} ${styles.open} bg-neutral-800 h-screen overflow-hidden top-12 left-0 flex-col`}
+        >
+          <div className="p-3 flex justify-end text-white">
+            <Button
+              className="cursor-pointer flex flex-row items-center bg-neutral-800"
+              onClick={() => setShowSideBar(!showSideBar)}
+              icon={<XMarkIcon />}
+              iconPosition="right"
+              variant="primary-neutral"
+            >
+              Lukk
+            </Button>
+          </div>
+          <ul className="px-6 top-1.5 flex flex-col space-y-2 text-white">
+            <Link className={`flex flex-row items-center ${styles.link}`} to={ROUTE_PATH.SOKOS_UP_HOME}>
+              <HouseIcon className="h-6 w-6 mr-2 mb-1" />
+              Hjem
+            </Link>
+            <div className="border-b border-gray-600 my-2" />
+            {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_MIKROFRONTEND_READ) && (
+              <>
+                <Link className={styles.link} to={ROUTE_PATH.SOKOS_MIKROFRONTEND_TEMPLATE}>
+                  Mikrofrontend
+                </Link>
+                <div className="border-b border-gray-600 my-2" />
+              </>
+            )}
+            {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_KRP_READ) && (
+              <>
+                <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_KRP}>
+                  Kontoregister person kontosøk
+                </Link>
+                <div className="border-b border-gray-600 my-2" />
+              </>
+            )}
+            {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_ORS_READ) && (
+              <>
+                <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_ORS}>
+                  Oppslag Reskontro Stønad
+                </Link>
+                <div className="border-b border-gray-600 my-2" />
+              </>
+            )}
+            {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_SKATTEKORT_READ) && (
+              <>
+                <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_SKATTEKORT}>
+                  Skattekort
+                </Link>
+                <div className="border-b border-gray-600 my-2" />
+              </>
+            )}
+            {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_OPPDRAGSINFO_READ) && (
+              <>
+                <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_OPPDRAGSINFO}>
+                  Oppdragsinfo
+                </Link>
+                <div className="border-b border-gray-600 my-2" />
+              </>
+            )}
+          </ul>
         </div>
-        <ul className="px-6 top-1.5 flex flex-col space-y-2 text-white">
-          <Link className={`flex flex-row items-center ${styles.link}`} to={ROUTE_PATH.SOKOS_UP_HOME}>
-            <HouseIcon className="h-6 w-6 mr-2 mb-1" />
-            Hjem
-          </Link>
-          <div className="border-b border-gray-600 my-2" />
-          {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_MIKROFRONTEND_READ) && (
-            <>
-              <Link className={styles.link} to={ROUTE_PATH.SOKOS_MIKROFRONTEND_TEMPLATE}>
-                Mikrofrontend
-              </Link>
-              <div className="border-b border-gray-600 my-2" />
-            </>
-          )}
-          {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_KRP_READ) && (
-            <>
-              <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_KRP}>
-                Kontoregister person kontosøk
-              </Link>
-              <div className="border-b border-gray-600 my-2" />
-            </>
-          )}
-          {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_ORS_READ) && (
-            <>
-              <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_ORS}>
-                Oppslag Reskontro Stønad
-              </Link>
-              <div className="border-b border-gray-600 my-2" />
-            </>
-          )}
-          {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_SKATTEKORT_READ) && (
-            <>
-              <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_SKATTEKORT}>
-                Skattekort
-              </Link>
-              <div className="border-b border-gray-600 my-2" />
-            </>
-          )}
-          {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_OPPDRAGSINFO_READ) && (
-            <>
-              <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_OPPDRAGSINFO}>
-                Oppdragsinfo
-              </Link>
-              <div className="border-b border-gray-600 my-2" />
-            </>
-          )}
-        </ul>
-      </div>
-      {!showSideBar && (
-        <div className="bg-neutral-800 w-20 left-0 h-screen flex flex-col justify-start items-center">
+      ) : (
+        <div className={`${styles.sidebar} bg-neutral-800 left-0 h-screen flex flex-col justify-start items-center`}>
           <Button
-            className="cursor-pointer bg-neutral-800"
+            className="cursor-pointer mt-2 bg-neutral-800"
             onClick={() => setShowSideBar(!showSideBar)}
             variant="primary-neutral"
             icon={<MenuHamburgerIcon className="w-8 h-8" />}
