71b995c353ca6135ea5ee25d8bc0f278295448be
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-03-15 11:28:56 +0100

    Holder dette?

--- src/models/routePath.ts
+++ src/models/routePath.ts
@@ -1,5 +1,5 @@
 export const ROUTE_PATH = {
-  SOKOS_MIKROFRONTEND_TEMPLATE: "/mikrofrontend",
+  SOKOS_MIKROFRONTEND_TEMPLATE: "/mikrofrontend/*",
   SOKOS_UP_ORS: "/ors",
   SOKOS_UP_SKATTEKORT: "/skattekort",
   SOKOS_UP_OPPDRAGSINFO: "/oppdragsinfo",
