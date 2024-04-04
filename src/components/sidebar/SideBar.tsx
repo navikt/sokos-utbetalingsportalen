c9af741befbf458c0a7db285a8252d1a10e2bb18
jonasnico <jonas.nicolaysen@nav.no>
2024-04-04 12:37:19 +0200

    gjør at linkene/teksten i sidebar ikke rører på seg når man åpner den

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -26,64 +26,66 @@ const SideBar = () => {
         <div
           className={`${styles.sidebar} ${styles.open} bg-neutral-800 min-h-screen overflow-hidden top-12 left-0 flex-col`}
         >
-          <div className="p-3 flex justify-end text-white">
-            <Button
-              className="cursor-pointer flex flex-row items-center bg-neutral-800"
-              onClick={() => setShowSideBar(!showSideBar)}
-              icon={<XMarkIcon />}
-              iconPosition="right"
-              variant="primary-neutral"
-            >
-              Lukk
-            </Button>
+          <div className={styles.textContainer}>
+            <div className="p-3 flex justify-end text-white">
+              <Button
+                className="cursor-pointer flex flex-row items-center bg-neutral-800"
+                onClick={() => setShowSideBar(!showSideBar)}
+                icon={<XMarkIcon />}
+                iconPosition="right"
+                variant="primary-neutral"
+              >
+                Lukk
+              </Button>
+            </div>
+            <ul className="px-6 top-1.5 flex flex-col space-y-2 text-white">
+              <Link className={`flex flex-row items-center ${styles.link}`} to={ROUTE_PATH.SOKOS_UP_HOME}>
+                <HouseIcon className="h-6 w-6 mr-2 mb-1" />
+                Hjem
+              </Link>
+              <div className="border-b border-gray-600 my-2" />
+              {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_MIKROFRONTEND_READ) && (
+                <>
+                  <Link className={styles.link} to={ROUTE_PATH.SOKOS_MIKROFRONTEND_TEMPLATE}>
+                    Mikrofrontend
+                  </Link>
+                  <div className="border-b border-gray-600 my-2" />
+                </>
+              )}
+              {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_KRP_READ) && (
+                <>
+                  <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_KRP}>
+                    Kontoregister person kontosøk
+                  </Link>
+                  <div className="border-b border-gray-600 my-2" />
+                </>
+              )}
+              {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_ORS_READ) && (
+                <>
+                  <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_ORS}>
+                    Oppslag Reskontro Stønad
+                  </Link>
+                  <div className="border-b border-gray-600 my-2" />
+                </>
+              )}
+              {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_SKATTEKORT_READ) && (
+                <>
+                  <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_SKATTEKORT}>
+                    Skattekort
+                  </Link>
+                  <div className="border-b border-gray-600 my-2" />
+                </>
+              )}
+              {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_OPPDRAGSINFO_READ) && (
+                <>
+                  <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_OPPDRAGSINFO}>
+                    Oppdragsinfo
+                  </Link>
+                  <div className="border-b border-gray-600 my-2" />
+                </>
+              )}
+            </ul>
           </div>
-          <ul className="px-6 top-1.5 flex flex-col space-y-2 text-white">
-            <Link className={`flex flex-row items-center ${styles.link}`} to={ROUTE_PATH.SOKOS_UP_HOME}>
-              <HouseIcon className="h-6 w-6 mr-2 mb-1" />
-              Hjem
-            </Link>
-            <div className="border-b border-gray-600 my-2" />
-            {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_MIKROFRONTEND_READ) && (
-              <>
-                <Link className={styles.link} to={ROUTE_PATH.SOKOS_MIKROFRONTEND_TEMPLATE}>
-                  Mikrofrontend
-                </Link>
-                <div className="border-b border-gray-600 my-2" />
-              </>
-            )}
-            {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_KRP_READ) && (
-              <>
-                <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_KRP}>
-                  Kontoregister person kontosøk
-                </Link>
-                <div className="border-b border-gray-600 my-2" />
-              </>
-            )}
-            {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_ORS_READ) && (
-              <>
-                <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_ORS}>
-                  Oppslag Reskontro Stønad
-                </Link>
-                <div className="border-b border-gray-600 my-2" />
-              </>
-            )}
-            {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_SKATTEKORT_READ) && (
-              <>
-                <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_SKATTEKORT}>
-                  Skattekort
-                </Link>
-                <div className="border-b border-gray-600 my-2" />
-              </>
-            )}
-            {hasAccess(AzureAdGroupName.AD_GRUPPE_SOKOS_MF_OPPDRAGSINFO_READ) && (
-              <>
-                <Link className={styles.link} to={ROUTE_PATH.SOKOS_UP_OPPDRAGSINFO}>
-                  Oppdragsinfo
-                </Link>
-                <div className="border-b border-gray-600 my-2" />
-              </>
-            )}
-          </ul>
         </div>
       ) : (
         <div
