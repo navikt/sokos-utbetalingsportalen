567fc65f2f5a757e3f1ed2ac9974936ad7983075
jonasnico <jonas.nicolaysen@nav.no>
2024-04-08 18:29:53 +0200

    bruker egen component for appcards

---
+++ src/components/appcard/AppCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Box, BodyShort, Tooltip, Link } from "@navikt/ds-react";
+import { ChevronRightIcon } from "@navikt/aksel-icons";
+import { ROUTE_PATH } from "../../models/routePath";
+import { Link as ReactRouterLink } from "react-router-dom";
+import styles from "./AppCard.module.css";
+
+interface AppCardProps {
+  hasAccess: boolean;
+  route: keyof typeof ROUTE_PATH;
+  title: string;
+}
+
+const AppCard: React.FC<AppCardProps> = ({ hasAccess, route, title }) => {
+  if (hasAccess) {
+    return (
+      <Link as={ReactRouterLink} to={route} variant="neutral" underline={false}>
+        <Box background="surface-alt-2-subtle" padding="6" shadow="medium" borderRadius="xlarge">
+          <div className={styles.apperContent}>
+            <BodyShort weight="semibold">{title}</BodyShort>
+            <ChevronRightIcon />
+          </div>
+        </Box>
+      </Link>
+    );
+  } else {
+    return (
+      <Tooltip content="Du har ikke tilgang til denne appen">
+        <Box background="surface-alt-2-subtle" padding="6" shadow="medium" borderRadius="xlarge">
+          <div className={styles.apperContent}>
+            <BodyShort weight="semibold">{title}</BodyShort>
+            <ChevronRightIcon />
+          </div>
+        </Box>
+      </Tooltip>
+    );
+  }
+};
+
+export default AppCard;
