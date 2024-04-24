e85aa9625f9b19f0037e1fee55ec95d226daeee1
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-04-24 15:26:44 +0200

    tester å fjerne eslint

--- src/Mikrofrontend.tsx
+++ src/Mikrofrontend.tsx
@@ -2,7 +2,6 @@ import React, { LazyExoticComponent } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import FeilMelding from "./components/feilmelding/Feilmelding";
 import ContentLoader from "./components/loader/ContentLoader";
-import useStore, { selectGjelderID } from "./store/store";
 import { useLocation } from "react-router-dom";
 
 type MikrofrontendProps = {
@@ -18,14 +17,13 @@ const createMikrofrontendBundle = (url: string): LazyExoticComponent<React.Compo
   return React.lazy(() => import(/* @vite-ignore */ url));
 };
 
-const Mikrofrontend: React.FC<MikrofrontendProps> = ({ url, includeGjelderId = false }) => {
-  const gjelderId = useStore(selectGjelderID);
+const Mikrofrontend: React.FC<MikrofrontendProps> = ({ url }) => {
   const MikrofrontendBundle = createMikrofrontendBundle(url);
   const location = useLocation();
   return (
     <React.Suspense fallback={<ContentLoader />}>
       <ErrorBoundary FallbackComponent={FeilMelding} key={location.pathname}>
-        <MikrofrontendBundle gjelderId={includeGjelderId ? gjelderId : null} />
+        <MikrofrontendBundle />
       </ErrorBoundary>
     </React.Suspense>
   );
