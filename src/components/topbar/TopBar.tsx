dc30c358059fac8ee040906bb15fb58c1a71b498
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-04-30 10:15:29 +0200

    fjerner gjelderId og alle spor av eslint

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -1,19 +1,10 @@
 import { Dropdown, InternalHeader as Header } from "@navikt/ds-react";
 import { Link, useLoaderData } from "react-router-dom";
-import useStore, { selectGjelderID, selectSetGjelderID } from "../../store/store";
 import nav from "../../../assets/images/Hvit.png";
-import { Label, TextField } from "@navikt/ds-react";
 import { UserData } from "../../models/userData";
 
 const TopBar = () => {
-  // const setGjelderId = useStore(selectSetGjelderID);
   const userInfo = useLoaderData() as UserData;
-  // const gjelderId = useStore(selectGjelderID);
-
-  // const handleGjelderIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setGjelderId(event.target.value);
-  // };
-
   return (
     <Header>
       <div className="w-16 p-1 ml-3">
@@ -22,12 +13,6 @@ const TopBar = () => {
       <Header.Title as={Link} to={"/"} className="border-none">
         Utbetalingsportalen
       </Header.Title>
-      {/*<div id="gjelder-id" className="w-fit flex justify-end items-stretch space-x-7 p-2">*/}
-      {/*  <div id="gjelder-id-tekst" className="flex justify-center items-center">*/}
-      {/*    <Label>GjelderID: </Label>*/}
-      {/*  </div>*/}
-      {/*  <TextField label="" hideLabel size="small" onBlur={handleGjelderIdChange} />*/}
-      {/*</div>*/}
       <Dropdown>
         <Header.UserButton
           as={Dropdown.Toggle}
