c88a972833c74d7ca4f39f81166912d5cc7c06bf
jonasnico <jonas.nicolaysen@nav.no>
2024-05-08 10:15:47 +0200

    :package: legger til eslint config for prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,8 @@
 // @ts-check
+import eslintConfigPrettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(...tseslint.configs.recommended);
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+);
