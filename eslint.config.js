3b6beeb0f4368db2bf903f6e9c12456cf0e8d8d1
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-05-10 16:02:52 +0200

    Fikser eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,22 @@
-// @ts-check
+import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   ...tseslint.configs.recommended,
+  js.configs.recommended,
   eslintConfigPrettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-console": "error",
+      "no-duplicate-imports": "error",
+    },
+  },
 );
