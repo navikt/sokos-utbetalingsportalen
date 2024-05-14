128dc3681c766a4b5914f7b0ea6588a23507fa9e
mudasar187 <mudasar_cheema@hotmail.com>
2024-05-14 14:19:23 +0200

    legger til 2 nye eslint plugins og oppdaterer name og description i package.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import eslint from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -14,7 +16,13 @@ export default tseslint.config(
         ...globals.node,
       },
     },
+    plugins: {
+      "jsx-a11y": jsxA11y,
+      "react-hooks": reactHooks,
+    },
     rules: {
+      ...jsxA11y.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
       "no-console": "error",
       "no-duplicate-imports": "error",
     },
