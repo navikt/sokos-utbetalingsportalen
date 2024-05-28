f05d5292c7c05aee2eb45acfd103384b18730588
jonasnico <jonas.nicolaysen@nav.no>
2024-05-28 12:01:09 +0200

    tester å vise feilmeldingen til bruker

--- src/components/errormessage/ErrorMessage.tsx
+++ src/components/errormessage/ErrorMessage.tsx
@@ -1,12 +1,30 @@
-import { Alert } from "@navikt/ds-react";
+import { Alert, BodyShort } from "@navikt/ds-react";
 import styles from "./ErrorMessage.module.css";
 
 const ErrorMessage = (props: { error: Error }) => (
   <>
     <Alert variant="error" className={styles.errormessage}>
-      {props.error.message
-        ? "En feil har skjedd, meld inn sak i Porten"
-        : "Noe gikk galt, prøv igjen. Hvis problemet vedvarer, meld inn sak i Porten"}
+      {props.error.message ? (
+        <>
+          <BodyShort size="medium" spacing>
+            En feil har skjedd, meld inn sak i Porten
+          </BodyShort>
+          <BodyShort size="medium" spacing>
+            Feilmelding: {props.error.message}
+            {props.error.cause ? (
+              <>
+                <br />
+                Årsak: {props.error.cause}
+              </>
+            ) : null}
+          </BodyShort>
+        </>
+      ) : (
+        <BodyShort size="medium" spacing>
+          Noe gikk galt, prøv igjen. Hvis problemet vedvarer, meld inn sak i
+          Porten
+        </BodyShort>
+      )}
     </Alert>
   </>
 );
