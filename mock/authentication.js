4bc718ce0e7f8830966af4893108429598832186
Haimanot Tekie <haimanot.tekie@nav.no>
2024-08-27 13:52:44 +0200

    Gi tilgang til ressending til bank i lokal kjøring

--- mock/authentication.js
+++ mock/authentication.js
@@ -12,6 +12,7 @@ export default [
           "b01fb216-fcb3-4ede-b7da-71fffe859763",
           "a13b4176-e328-4e1c-b181-ff676a7146b1",
           "e0023d91-26bc-4d5d-95ba-3148b6123afc",
+          "391bec9e-e71e-42cb-a030-56c394dd13fd"
         ],
       };
     },
