720b758c979966ce82f436b18e5a55d5dc6c2863
takwaiwang <tak.wai.wang@nav.no>
2024-09-19 09:49:15 +0200

    Test with new errorboundary fix

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,15 +1,36 @@
-import { ReactNode } from "react";
-import { ErrorBoundary as ReactErrorBoundary } from "react-error-boundary";
+import React, { ErrorInfo } from "react";
 import ErrorPage from "../pages/ErrorPage";
 
 interface Props {
-  children: ReactNode;
+  children: React.ReactNode;
 }
 
-export function ErrorBoundary({ children }: Props) {
-  return (
-    <ReactErrorBoundary FallbackComponent={() => <ErrorPage />}>
-      {children}
-    </ReactErrorBoundary>
-  );
+interface State {
+  hasError: boolean;
+  error?: Error;
+  errorInfo?: ErrorInfo;
 }
+
+class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.setState({ hasError: true, error, errorInfo });
+  }
+
+  render(): React.ReactNode {
+    if (this.state.hasError) {
+      return <ErrorPage />;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
