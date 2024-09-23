951774155952c8a21492755f25216286bf5a146e
Mudasar Cheema <mudasar_cheema@hotmail.com>
2024-09-23 11:25:21 +0200

    test uten basepath (#328)
    * test uten basepath
    
    * endre url for kontoregister-organisasjon

--- src/models/routePath.ts
+++ src/models/routePath.ts
@@ -7,5 +7,9 @@ export const ROUTE_PATH = {
   SOKOS_UP_OPPDRAGSINFO: "/oppdragsinfo",
   SOKOS_UP_KRP: "/krp",
   SOKOS_UP_RESENDING_BANK: "/resending-bank",
-  SOKOS_UP_KRO: "/kro",
+  SOKOS_UP_KRO: "/kontoregister-organisasjon",
 } as const;
+
+// NB! Forkortelser i URL er ikke en god idé. Det er bedre å bruke hele ord.
+// NB! Det er en god idé å bruke små bokstaver i URL.
+// NB! Det er en god idé å bruke bindestrek i URL for å skille på ord.
