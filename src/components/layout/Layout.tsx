2681efab0581e4a6d701ce1dfa0af87d27dc3e78
Jonas Nicolaysen <70371917+jonasnico@users.noreply.github.com>
2024-10-31 21:16:10 +0100

    Vis banner hvis man er i dev (#351)
    * Vis headertitle basert på cluster
    
    * Prøv å sett env variabelen
    
    * Henter hostname og legger banner i layout
    
    * Trenger ikke jsx fragment rundt
    
    * Revert space
    
    * Få topbar til å følge hele bredden horisontalt
    
    * Prøver igjen
    
    * liten fix på useState
    
    * Revert topbar styling
    
    ---------
    
    Co-authored-by: mudasar187 <mudasar_cheema@hotmail.com>

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,20 +1,36 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import { getEnvironment } from "../../utils/environment";
 import SideBar from "../sidebar/SideBar";
 import TopBar from "../topbar/TopBar";
 import styles from "./Layout.module.css";
 
 type LayoutProps = {
-  children: ReactNode; // Define the type for children
+  children: ReactNode;
 };
 
 export default function Layout({ children }: LayoutProps) {
+  const [environment, setEnvironment] = useState<string | null>(null);
+
+  useEffect(() => {
+    const environment = getEnvironment();
+    setEnvironment(environment);
+  }, []);
+
   return (
     <>
       <TopBar />
       <div className={styles["utbetalingsportalen-body"]}>
         <SideBar />
-
-        <div className={styles["utbetalingsportalen-outlet"]}>{children} </div>
+        <div className={styles["utbetalingsportalen-outlet"]}>
+          {environment === "development" && (
+            <div className={styles["utbetalingsportalen-banner"]}>
+              <span>TEST</span>
+            </div>
+          )}
+          <div className={styles["utbetalingsportalen-content"]}>
+            {children}
+          </div>
+        </div>
       </div>
     </>
   );
