7634b6e3cc84c806253290bb4ca13aacf5794903
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2024-10-31 18:42:24 +0100

    Henter hostname og legger banner i layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import { getEnvironment } from "../../utils/environment";
 import SideBar from "../sidebar/SideBar";
 import TopBar from "../topbar/TopBar";
 import styles from "./Layout.module.css";
@@ -8,13 +9,28 @@ type LayoutProps = {
 };
 
 export default function Layout({ children }: LayoutProps) {
+  const [environment, setEnvironment] = useState<string>("");
+
+  useEffect(() => {
+    const environment = getEnvironment();
+    setEnvironment(environment);
+  }, []);
+
   return (
     <>
       <TopBar />
       <div className={styles["utbetalingsportalen-body"]}>
         <SideBar />
-
-        <div className={styles["utbetalingsportalen-outlet"]}>{children} </div>
+        <div className={styles["utbetalingsportalen-outlet"]}>
+          {environment === "development" && (
+            <div className={styles["utbetalingsportalen-banner"]}>
+              <span>TEST</span>
+            </div>
+          )}
+          <div className={styles["utbetalingsportalen-content"]}>
+            {children}
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { InternalHeader, Spacer } from "@navikt/ds-react";
 import nav from "../../../assets/images/Hvit.png";
@@ -6,38 +5,26 @@ import { useAuthContext } from "../../auth/userAuth";
 import styles from "./TopBar.module.css";
 
 export default function TopBar() {
-  const [title, setTitle] = useState("Utbetalingsportalen");
-
-  useEffect(() => {
-    const hostname = window.location.hostname;
-    const clusterName = import.meta.env.VITE_NAIS_CLUSTER_NAME;
-
-    if (hostname === "localhost") {
-      setTitle("Utbetalingsportalen - Lokal");
-    } else if (clusterName === "dev-gcp") {
-      setTitle("Utbetalingsportalen - Dev");
-    } else if (clusterName === "prod-gcp") {
-      setTitle("Utbetalingsportalen - Prod");
-    }
-  }, []);
-
   const authContext = useAuthContext();
+
   return (
-    <InternalHeader>
-      <div className={styles["topbar-logo"]}>
-        <img src={nav} alt="NAV logo" />
-      </div>
-      <InternalHeader.Title as={Link} to={"/"} className="border-none">
-        {title}
-      </InternalHeader.Title>
-      <a href="#main-content" className="sr-only">
-        Hopp til hovedinnhold
-      </a>
-      <Spacer />
-      <InternalHeader.User
-        name={authContext.userData.name}
-        description={authContext.userData.navIdent}
-      />
-    </InternalHeader>
+    <>
+      <InternalHeader>
+        <div className={styles["topbar-logo"]}>
+          <img src={nav} alt="NAV logo" />
+        </div>
+        <InternalHeader.Title as={Link} to={"/"} className="border-none">
+          Utbetalingsportalen
+        </InternalHeader.Title>
+        <a href="#main-content" className="sr-only">
+          Hopp til hovedinnhold
+        </a>
+        <Spacer />
+        <InternalHeader.User
+          name={authContext.userData.name}
+          description={authContext.userData.navIdent}
+        />
+      </InternalHeader>
+    </>
   );
 }
