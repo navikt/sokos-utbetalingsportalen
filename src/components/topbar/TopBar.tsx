830dd2c87660d2a741016af4170c09d542b90a2a
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2024-10-31 17:05:41 +0100

    Vis headertitle basert på cluster

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { InternalHeader, Spacer } from "@navikt/ds-react";
 import nav from "../../../assets/images/Hvit.png";
@@ -5,6 +6,21 @@ import { useAuthContext } from "../../auth/userAuth";
 import styles from "./TopBar.module.css";
 
 export default function TopBar() {
+  const [title, setTitle] = useState("Utbetalingsportalen");
+
+  useEffect(() => {
+    const hostname = window.location.hostname;
+    const clusterName = import.meta.env.VITE_NAIS_CLUSTER_NAME;
+
+    if (hostname === "localhost") {
+      setTitle("Utbetalingsportalen - Lokal");
+    } else if (clusterName === "dev-gcp") {
+      setTitle("Utbetalingsportalen - Dev");
+    } else if (clusterName === "prod-gcp") {
+      setTitle("Utbetalingsportalen - Prod");
+    }
+  }, []);
+
   const authContext = useAuthContext();
   return (
     <InternalHeader>
@@ -12,7 +28,7 @@ export default function TopBar() {
         <img src={nav} alt="NAV logo" />
       </div>
       <InternalHeader.Title as={Link} to={"/"} className="border-none">
-        Utbetalingsportalen
+        {title}
       </InternalHeader.Title>
       <a href="#main-content" className="sr-only">
         Hopp til hovedinnhold
