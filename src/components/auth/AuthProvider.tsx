87375aa318a1e5ea772821ca17310ed95b8247f7
mudasar187 <mudasar_cheema@hotmail.com>
2024-11-28 13:40:40 +0100

    refaktorerer og håndtering av error osv på en bedre måte

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -3,42 +3,60 @@ import { useLocation } from "react-router-dom";
 import { Loader } from "@navikt/ds-react";
 import { ApiError } from "../../types/ApiError";
 import { UserData } from "../../types/UserData";
+import { logFaroError } from "../../utils/grafanaFaro";
 import { AuthContext } from "./AuthContext";
 
 export function AuthProvider(props: PropsWithChildren) {
-  const [userData, setUserData] = useState<UserData>();
+  const [userData, setUserData] = useState<UserData | null>(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState(true);
   const location = useLocation();
 
   async function authenticateUser() {
     try {
       const response = await fetch("/userinfo");
-      const data = await response.json();
+      if (!response.ok) {
+        const error = new Error("Failed to fetch user data from server");
+        logFaroError(error);
+        throw error;
+      }
+      const data = await response.json().catch((err) => {
+        const error = new Error(err);
+        logFaroError(error);
+        throw error;
+      });
       if (data.error) {
-        setError(new ApiError("Failed to fetch user data", data.error));
+        const error = new Error("User data contains an error");
+        logFaroError(error);
+        throw error;
       }
       setUserData(data);
       setIsAuthenticated(true);
     } catch (error) {
-      setError(new ApiError("Internal server error, " + error));
+      setError(new ApiError(`Internal server error: ${error}`));
+    } finally {
+      setLoading(false);
     }
   }
 
   useEffect(() => {
-    authenticateUser().catch((error) => {
-      setError(error);
-    });
+    setLoading(true);
+    authenticateUser();
   }, [location]);
 
-  if (error) {
-    throw error;
-  }
-
-  if (!isAuthenticated) {
+  if (loading) {
     return (
       <div className="w-full text-center py-16">
         <Loader size="2xlarge" title="Venter..." transparent />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="w-full text-center py-16">
+        <p>Error: {error.message}</p>
       </div>
     );
   }
