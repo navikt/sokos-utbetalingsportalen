6e8a6be9be2bee70fbc0b635e885a2e6e1428c17
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-01-03 10:42:45 +0100

    custom button og én state for feedback

--- src/components/tilbakemelding/Tilbakemelding.tsx
+++ src/components/tilbakemelding/Tilbakemelding.tsx
@@ -1,56 +1,75 @@
 import React, { useRef, useState } from "react";
-import { EnvelopeClosedIcon } from "@navikt/aksel-icons";
 import { Button, Modal, Select, TextField } from "@navikt/ds-react";
 import {
   MicrofrontendApp,
   MicrofrontendConfig,
 } from "../../config/microfrontend";
+import styles from "./Tilbakemelding.module.css";
 
 type Props = {
   showTilbakemelding: boolean;
   setShowTilbakemelding: (show: boolean) => void;
 };
 
+type FeedbackData = {
+  feedback: string;
+  app: string;
+};
+
 export default function Tilbakemelding(props: Props) {
   const ref = useRef<HTMLDialogElement>(null);
-  const [feedback, setFeedback] = useState<string>("");
-  const [app, setApp] = useState<string>("");
+  const [feedbackData, setFeedbackData] = useState<FeedbackData>({
+    feedback: "",
+    app: "",
+  });
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     // eslint-disable-next-line no-console
-    console.log("Tilbakemelding sendt", { feedback, app });
-    alert("Tilbakemelding sendt: " + feedback + " " + app);
+    console.log("Tilbakemelding sendt", feedbackData);
+    alert("Tilbakemelding sendt: " + JSON.stringify(feedbackData));
     ref.current?.close();
     props.setShowTilbakemelding(false);
   };
 
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    const { name, value } = e.target;
+    setFeedbackData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div>
-      <Button
-        className="rounded-3xl"
-        icon={<EnvelopeClosedIcon title={"Tilbakemelding"} />}
-        iconPosition="right"
+      <button
+        className={styles.button}
         onClick={() => ref.current?.showModal()}
-      ></Button>
+      >
+        Tilbakemelding?
+      </button>
 
       <Modal
         ref={ref}
         header={{ heading: "Gi din tilbakemelding" }}
-        width={400}
+        width={600}
       >
         <Modal.Body>
           <form method="dialog" id="skjema" onSubmit={handleSubmit}>
             <div className="flex flex-col gap-5">
               <TextField
                 label="Har du noen tilbakemeldinger?"
-                value={feedback}
-                onChange={(e) => setFeedback(e.target.value)}
+                name="feedback"
+                value={feedbackData.feedback}
+                onChange={handleChange}
               />
               <Select
                 label={"Hvilken applikasjon gjelder det?"}
-                value={app}
-                onChange={(e) => setApp(e.target.value)}
+                name="app"
+                value={feedbackData.app}
+                onChange={handleChange}
               >
                 <option key={""} value={""}>
                   - Velg applikasjon -
