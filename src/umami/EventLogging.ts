3b75ddfe52efa23956b8f39d8bf087dd8588f1fe
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-01-10 13:53:52 +0100

    Alfabetiske events

--- src/umami/EventLogging.ts
+++ src/umami/EventLogging.ts
@@ -2,8 +2,8 @@ export const EVENT_NAME = {
   AAPNE_APP_SWITCHER: "App switcher åpnet",
   AAPNE_ARBEIDSFLATE: "Åpnet arbeidsflate fra App switcher",
   AAPNE_SIDEBAR: "Sidebar åpnet",
-  LUKK_SIDEBAR: "Sidebar lukket",
-  VIS_ALLE_APPER: "Vis alle apper toggled",
   APPCARD_TRYKKET: "Appcard trykket",
+  LUKK_SIDEBAR: "Sidebar lukket",
   SIDEBAR_LINK_TRYKKET: "Sidebar link trykket",
+  VIS_ALLE_APPER: "Vis alle apper toggled",
 };
