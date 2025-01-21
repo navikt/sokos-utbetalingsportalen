ed36ee1893831f9dd810994e39f088a4ec648167
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-01-21 12:52:44 +0100

    Close the sidebar by default

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -43,7 +43,7 @@ export default function Layout({ children }: LayoutProps) {
     environment: "",
     applicationEnvironment: "",
   });
-  const [showSideBar, setShowSideBar] = useState(true);
+  const [showSideBar, setShowSideBar] = useState(false);
 
   useEffect(() => {
     const applicationEnvironment = getApplicationEnvrionment();
