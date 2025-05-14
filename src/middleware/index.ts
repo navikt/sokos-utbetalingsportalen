a31e7db68ee8bd51a60b294f7faf4bee8ba0c913
mudasar187 <mudasar_cheema@hotmail.com>
2025-05-14 13:50:08 +0200

    oppdatere route
    Co-authored-by: bjornko <bjorn.vaggen.konestabo@nav.no>

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -26,6 +26,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
         "48a80bbb-be45-4ef6-aab8-21604f057f47", // 0000-GA-SOKOS-MF-Venteregister
         "2020a765-ffae-4042-b4cc-2a5a783a3ec5", // 0000-GA-SOKOS-MF-Meldingsflyt-READ
         "7e0c2ad1-d0e7-4fa8-8169-7a9d68435644", // 0000-GA-SOKOS-MF-Fastedata-READ
+        "2477057d-7f80-4517-a885-20c948bf0367", // 0000-GA-SOKOS-MF-DARE-POC
       ],
     };
     return next();
