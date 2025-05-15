0b905071dcd0c0a7913f35e72dd3e18ba3191ff9
mudasar187 <mudasar_cheema@hotmail.com>
2025-05-15 14:40:45 +0200

    fjerne importmap
    Co-authored-by: Jonas Nicolaysen <jonas.nicolaysen@nav.no>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
 import { defineConfig } from "astro/config";
-import { rollupImportMapPlugin } from "rollup-plugin-import-map";
-import importmap from "./importmap.json";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
@@ -9,23 +7,7 @@ export default defineConfig({
   /*build: {
     assetsPrefix: "https://cdn.nav.no/min-side/utbetalingsportalen", // Change this to your CDN prefix
   },*/
-  integrations: [
-    react(),
-    {
-      name: "importmap",
-      hooks: {
-        "astro:build:setup": ({ vite, target }) => {
-          if (target === "client") {
-            vite.plugins.push({
-              ...rollupImportMapPlugin(importmap),
-              enforce: "pre",
-              apply: "build",
-            });
-          }
-        },
-      },
-    },
-  ],
+  integrations: [react()],
   output: "server",
   adapter: node({
     mode: "standalone",
