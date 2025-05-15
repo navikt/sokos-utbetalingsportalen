705f59d936c7d1fc26752bfbf3f7a9ff436e3404
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-05-15 14:55:08 +0200

    Sett react som external

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,19 @@ export default defineConfig({
   /*build: {
     assetsPrefix: "https://cdn.nav.no/min-side/utbetalingsportalen", // Change this to your CDN prefix
   },*/
-  integrations: [react()],
+  integrations: [
+    react(),
+    {
+      name: 'importmap-externals',
+      hooks: {
+        'astro:build:setup': ({ vite, target }) => {
+          if(target === 'client') {
+            vite.build.rollupOptions["external"] = ["react", "react-dom"];
+          }
+        }
+      }
+    },
+  ],
   output: "server",
   adapter: node({
     mode: "standalone",
