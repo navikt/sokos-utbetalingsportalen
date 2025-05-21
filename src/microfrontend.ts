08fa256c45b011f4ad3b124a7b1a365398723f46
mudasar187 <mudasar_cheema@hotmail.com>
2025-05-21 10:53:16 +0200

    refaktorering av hvordan vi henter bundleurl
    Co-authored-by: Jonas Nicolaysen <jonas.nicolaysen@nav.no>

--- src/microfrontend.ts
+++ src/microfrontend.ts
@@ -1,3 +1,12 @@
+import { getClientSideEnvironment } from "@utils/client/environments";
+
+function fetchMicrofrontendBundleUrl(appName: string) {
+  if (getClientSideEnvironment() === "local") {
+    return "http://localhost:3000/bundle.js";
+  }
+  return `https://${window.location.hostname}/${appName}/bundle.js`;
+}
+
 export type MicroFrontend = {
   app: string;
   title: string;
@@ -5,7 +14,7 @@ export type MicroFrontend = {
   adGroupDevelopment: string;
   adGroupProduction: string;
   route: string;
-  naisAppName: string;
+  bundleUrl: string;
 };
 
 type MicrofrontendDictionary = Record<string, MicroFrontend>;
@@ -18,7 +27,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "1b717a23-d376-471c-9fc6-356299fadc2b",
     adGroupProduction: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
     route: "/grensesnittmal",
-    naisAppName: "sokos-mikrofrontend-template",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-mikrofrontend-template"),
   },
   {
     app: "ATTESTASJON",
@@ -27,7 +36,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "0de8d01f-8ad0-4391-841c-55392956bc17",
     adGroupProduction: "ae51bc06-2f9a-47a4-b33d-f0825ecb9985",
     route: "/attestasjon",
-    naisAppName: "sokos-up-attestasjon",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-attestasjon"),
   },
   {
     app: "OPPDRAGSINFO",
@@ -36,7 +45,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "e0023d91-26bc-4d5d-95ba-3148b6123afc",
     adGroupProduction: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
     route: "/oppdragsinfo",
-    naisAppName: "sokos-up-oppdragsinfo",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-oppdragsinfo"),
   },
   {
     app: "VENTEREGISTER",
@@ -45,7 +54,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "48a80bbb-be45-4ef6-aab8-21604f057f47",
     adGroupProduction: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
     route: "/venteregister",
-    naisAppName: "sokos-up-venteregister",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-venteregister"),
   },
   {
     app: "FASTEDATA",
@@ -54,7 +63,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "7e0c2ad1-d0e7-4fa8-8169-7a9d68435644",
     adGroupProduction: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
     route: "/fastedata",
-    naisAppName: "sokos-up-fastedata",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-fastedata"),
   },
   {
     app: "SKATTEKORT",
@@ -63,7 +72,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "a13b4176-e328-4e1c-b181-ff676a7146b1",
     adGroupProduction: "6c93762e-ea37-4043-919f-3a758a136943",
     route: "/skattekort",
-    naisAppName: "sokos-up-skattekort",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-skattekort"),
   },
   {
     app: "SPK_MOTTAK",
@@ -72,7 +81,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "0e58dc41-7c57-4b79-a8c7-d0caec129e53",
     adGroupProduction: "f49b797d-600d-46da-8061-7746e48d1aca",
     route: "/spk-mottak",
-    naisAppName: "sokos-up-spk-mottak",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-spk-mottak"),
   },
   {
     app: "RESENDING-BANK",
@@ -81,7 +90,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "391bec9e-e71e-42cb-a030-56c394dd13fd",
     adGroupProduction: "de776092-a8ed-46a2-be5e-ec2eae4dcf2b",
     route: "/resending-bank",
-    naisAppName: "sokos-up-resending-bank",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-resending-bank"),
   },
   {
     app: "KRP",
@@ -90,7 +99,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "98146b9a-1891-44e3-9b61-92130c2fcd8b",
     adGroupProduction: "bb9c7baf-768b-4e84-816c-f10d0a6c7d25",
     route: "/kontoregister-person",
-    naisAppName: "sokos-up-krp",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-krp"),
   },
   {
     app: "KRO",
@@ -99,7 +108,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "bdcedce3-dab5-4b68-b1d3-8625cd0d3b55",
     adGroupProduction: "e1635067-dfc9-4fb8-8405-d50e9672cf5c",
     route: "/kontoregister-organisasjon",
-    naisAppName: "sokos-up-kro",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-kro"),
   },
   {
     app: "ORS",
@@ -108,7 +117,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "b01fb216-fcb3-4ede-b7da-71fffe859763",
     adGroupProduction: "25723642-e1da-4d15-87ce-8c4d2b87e33a",
     route: "/oppslag-reskontro-stoenad",
-    naisAppName: "sokos-up-ors",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-ors"),
   },
   {
     app: "UTBETALING",
@@ -117,7 +126,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "138d21fb-4e96-46d6-91e4-e3926aa349e5",
     adGroupProduction: "f8919276-46bf-43a2-a9cb-9a5c5f9c8e16",
     route: "/utbetaling",
-    naisAppName: "sokos-up-utbetaling",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-utbetaling"),
   },
   {
     app: "BUNTKONTROLL",
@@ -126,7 +135,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "3bc37bf2-8e76-407c-ad4a-d2c79edc241e",
     adGroupProduction: "08723132-a7ba-40eb-a6f4-0a9247f230d2",
     route: "/buntkontroll",
-    naisAppName: "sokos-up-buntkontroll",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-buntkontroll"),
   },
   {
     app: "MELDINGSFLYT",
@@ -135,7 +144,7 @@ export const microfrontendConfigArray: MicroFrontend[] = [
     adGroupDevelopment: "2020a765-ffae-4042-b4cc-2a5a783a3ec5",
     adGroupProduction: "ad41e055-af0c-4a26-a31c-c51d6ea236cd",
     route: "/meldingsflyt",
-    naisAppName: "sokos-up-meldingsflyt",
+    bundleUrl: fetchMicrofrontendBundleUrl("sokos-up-meldingsflyt"),
   },
 ];
 
