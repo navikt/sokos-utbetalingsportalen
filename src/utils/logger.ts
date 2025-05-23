bdb819d9e58d13671812f696f1c522b7fabdf8e3
mudasar187 <mudasar_cheema@hotmail.com>
2025-05-23 22:39:05 +0200

    legger til shutdown hook for tracing når appen stopper

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,6 +3,8 @@ import dayjs from "dayjs";
 import fs from "fs";
 import pino from "pino";
 
+// configure the SDK to export telemetry data to the console
+// enable all auto-instrumentations from the meta package
 const sdk = new NodeSDK({
   spanProcessor: new tracing.SimpleSpanProcessor(
     new tracing.ConsoleSpanExporter(),
@@ -12,7 +14,18 @@ const sdk = new NodeSDK({
   ),
 });
 
+// initialize the SDK and register with the OpenTelemetry API
+// this enables the API to record telemetry
 sdk.start();
+
+// gracefully shut down the SDK on process exit
+process.on("SIGTERM", () => {
+  sdk
+    .shutdown()
+    .then(() => logger.info("Tracing terminated"))
+    .catch((error) => logger.error("Error terminating tracing", error))
+    .finally(() => process.exit(0));
+});
 
 const secureLogPath = () =>
   fs.existsSync("/secure-logs/") ? "/secure-logs/secure.log" : "./secure.log";
