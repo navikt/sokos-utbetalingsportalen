c96169ca88dba573d700bd3d1c0d236d28edbb50
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-05-27 14:13:47 +0200

    følg errorpage eksempel fra aksel

--- src/components/microfrontend/MicrofrontendError.tsx
+++ src/components/microfrontend/MicrofrontendError.tsx
@@ -1,24 +1,49 @@
-import GoBack from "@components/go-back/GoBack";
-import { BodyShort, Box, Button, Heading, List } from "@navikt/ds-react";
+import {
+  BodyShort,
+  Box,
+  Button,
+  Heading,
+  Link,
+  List,
+  VStack,
+} from "@navikt/ds-react";
 import styles from "../../common-styles.module.css";
 
 export function MicrofrontendError() {
   return (
-    <Box paddingBlock="20 16" data-aksel-template="404-v2">
+    <Box paddingBlock="20 8">
       <div className={styles["error-page"]}>
-        <Heading level="1" size="large" spacing>
-          Beklager, noe gikk galt
-        </Heading>
-        <BodyShort>En teknisk feil gjør at siden er utilgjengelig.</BodyShort>
-        <List>
-          <List.Item>Du kan prøve å</List.Item>
-          <List.Item>
-            <GoBack fallbackUrl="/">Gå tilbake til forrige side</GoBack>
-          </List.Item>
-        </List>
-        <Button as="a" href="/">
-          Gå til hovedsiden
-        </Button>
+        <VStack gap="16">
+          <VStack gap="12" align="start">
+            <div>
+              <Heading level="1" size="large" spacing>
+                Beklager, noe gikk galt.
+              </Heading>
+              <BodyShort spacing>
+                En teknisk feil gjør at siden er utilgjengelig.
+              </BodyShort>
+              <BodyShort>Du kan prøve å</BodyShort>
+              <List>
+                <List.Item>
+                  vente noen minutter og{" "}
+                  <Link href="#" onClick={() => location.reload()}>
+                    laste siden på nytt
+                  </Link>
+                </List.Item>
+                <List.Item>
+                  {window.history.length > 1 && (
+                    <Link href="#" onClick={() => history.back()}>
+                      gå tilbake til forrige side
+                    </Link>
+                  )}
+                </List.Item>
+              </List>
+              <Button as="a" href="/">
+                Gå til hovedsiden
+              </Button>
+            </div>
+          </VStack>
+        </VStack>
       </div>
     </Box>
   );
