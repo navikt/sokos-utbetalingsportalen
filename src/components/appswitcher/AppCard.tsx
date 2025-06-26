3d2d65c2fe019ee96ab0835a29fe8f1bf2361c0d
Jonas Nicolaysen <70371917+jonasnico@users.noreply.github.com>
2025-06-26 14:58:03 +0200

    refactor: erstatt appcard implementasjon med den nye LinkCard komponenten (#558)

--- src/components/appswitcher/AppCard.tsx
+++ src/components/appswitcher/AppCard.tsx
@@ -1,5 +1,4 @@
-import { ChevronRightIcon } from "@navikt/aksel-icons";
-import { Heading, Tooltip } from "@navikt/ds-react";
+import { LinkCard, Tooltip } from "@navikt/ds-react";
 import styles from "./AppCard.module.css";
 
 type AppCardProps = {
@@ -11,32 +10,20 @@ type AppCardProps = {
 
 export default function AppCard(props: AppCardProps) {
   return props.hasAccess ? (
-    <a href={props.route} className={styles["appcard"]}>
-      <div className={styles["appcard__title"]}>
-        <Heading level="3" size="xsmall">
-          {props.title}
-        </Heading>
-        <div className={styles["appcard__arrow"]}>
-          <ChevronRightIcon title="Chevron ikon" />
-        </div>
-      </div>
-      <div className={styles["appcard__description"]}>{props.description}</div>
-    </a>
+    <LinkCard className={styles.appcard}>
+      <LinkCard.Title as="h3">
+        <LinkCard.Anchor href={props.route}>{props.title}</LinkCard.Anchor>
+      </LinkCard.Title>
+      <LinkCard.Description>{props.description}</LinkCard.Description>
+    </LinkCard>
   ) : (
     <Tooltip content="Du har ikke tilgang til denne appen">
-      <div className={`${styles["appcard"]} ${styles["appcard--disabled"]}`}>
-        <div className={styles["appcard__title"]}>
-          <Heading level="3" size="xsmall">
-            {props.title}
-          </Heading>
-          <div className={styles["appcard__arrow"]}>
-            <ChevronRightIcon title="Chevron ikon" />
-          </div>
-        </div>
-        <div className={styles["appcard__description"]}>
-          {props.description}
-        </div>
-      </div>
+      <LinkCard className={`${styles.appcard} ${styles["appcard--disabled"]}`}>
+        <LinkCard.Title as="h3">
+          <span>{props.title}</span>
+        </LinkCard.Title>
+        <LinkCard.Description>{props.description}</LinkCard.Description>
+      </LinkCard>
     </Tooltip>
   );
 }
