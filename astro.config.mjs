a54652a3526d050884f579916b6157846fde1fe2
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-08-17 17:17:43 +0200

    prøv passthroughImageService()

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
@@ -8,9 +8,7 @@ export default defineConfig({
     assetsPrefix: "https://cdn.nav.no/min-side/utbetalingsportalen", // TODO: oppdatere med vår cdn bucket
   },*/
   image: {
-    service: {
-      entrypoint: "astro/assets/services/sharp",
-    },
+    service: passthroughImageService(),
   },
   integrations: [
     react(),
