0c3c6bad11920031083b80a53bafa5e0f65b9b7f
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-10-31 19:26:33 +0100

    oppdater knip config for å bruke astro defaults

--- knip.ts
+++ knip.ts
@@ -1,9 +1,18 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  entry: ["src/pages/**/*.{astro,ts}", "src/middleware/index.ts"],
-  project: ["**/*.{ts,tsx,astro,js,jsx,mjs}"],
-  ignore: ["mock/**", "node_modules/**", "dist/**", ".astro/**"],
+  entry: [
+    "src/pages/**/*.{astro,ts,tsx}",
+    "!src/pages/**/_*",
+    "src/middleware/index.ts",
+  ],
+  ignore: [
+    "mock/**",
+    "node_modules/**",
+    "dist/**",
+    ".astro/**",
+    "playwright-report/**",
+  ],
 };
 
 export default config;
