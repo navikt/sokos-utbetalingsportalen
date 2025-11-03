856aa181e91ea1532596d06e141864164beb3293
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-11-03 14:33:03 +0100

    Viser riktige apper for prod

--- src/components/appswitcher/AppSwitcher.tsx
+++ src/components/appswitcher/AppSwitcher.tsx
@@ -4,17 +4,33 @@ import { useState } from "react";
 import { microfrontendConfigArray as allApps } from "src/microfrontend";
 import styles from "./AppSwitcher.module.css";
 import linkCardStyles from "./LinkCard.module.css";
+import { getClientSideEnvironment } from "@utils/client/environments";
 
 type AppSwitcherProps = {
   adGroups: string[];
 };
 
+const PLACEHOLDER_AD_GROUP = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+
+function getAvailableApps() {
+  const environment = getClientSideEnvironment();
+
+  if (environment === "production") {
+    return allApps.filter(
+      (app) => app.adGroupProduction !== PLACEHOLDER_AD_GROUP,
+    );
+  }
+
+  return allApps;
+}
+
 export default function AppSwitcher(props: AppSwitcherProps) {
   const authorizedApps = getAuthorizedApps(props.adGroups);
   const [showApps, setShowApps] = useState<string>("");
+  const availableApps = getAvailableApps();
 
   function appCards() {
-    return (showApps ? allApps : authorizedApps)
+    return (showApps ? availableApps : authorizedApps)
       .slice()
       .sort((a, b) => a.title.localeCompare(b.title))
       .map((app) => {
