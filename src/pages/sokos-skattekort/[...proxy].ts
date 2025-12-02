08d92052f2feb217a67eed7f97bedc144394bf4a
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2025-12-02 13:19:37 +0100

    Legger til sokos-skattekort som api proxy

---
+++ src/pages/sokos-skattekort/[...proxy].ts
@@ -0,0 +1,8 @@
+import type { APIRoute } from "astro";
+import { routeProxyWithOboToken } from "@utils/server/proxy";
+
+export const ALL: APIRoute = routeProxyWithOboToken({
+  apiProxy: `${process.env.SOKOS_SKATTEKORT_API_PROXY}`,
+  apiUrl: `${process.env.SOKOS_SKATTEKORT_API}`,
+  audience: `${process.env.SOKOS_SKATTEKORT_API_AUDIENCE}`,
+});
