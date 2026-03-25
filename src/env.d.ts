7443654dbee10e2d06d6db02146fb01a3fc8c793
Mudasar Cheema <mudasar_cheema@hotmail.com>
2026-03-25 11:59:03 +0100

    TOB-6433: oppgradere astro (#819)
    * oppgradere astro
    
    * logger ut url
    
    * tester allowedDomains
    
    * logge ut en del med astro oppgrdaering
    
    * teste endring av astro config
    
    * rydde bort logger
    
    * rydde bort import
    
    * fikse import type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-import { UserData } from "@types/UserData";
+import type { UserData } from "@types/UserData";
 
 declare global {
 	namespace App {
