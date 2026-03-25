077843173ac49c4b183b885844a213217b3d7619
mudasar187 <mudasar_cheema@hotmail.com>
2026-03-25 10:47:49 +0100

    teste endring av astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,16 +8,14 @@ export default defineConfig({
 		{
 			name: "importmap-externals",
 			hooks: {
-				"astro:build:setup": ({ vite, target }) => {
-					if (target === "client") {
-						vite.build.rollupOptions.external = [
-							"react",
-							"react/jsx-runtime",
-							"react-dom",
-							"react-dom/client",
-							"scheduler",
-						];
-					}
+				"astro:build:setup": ({ vite }) => {
+					vite.environments.client.build.rollupOptions.external = [
+						"react",
+						"react/jsx-runtime",
+						"react-dom",
+						"react-dom/client",
+						"scheduler",
+					];
 				},
 			},
 		},
