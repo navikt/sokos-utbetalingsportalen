05d430ec5b08f63e518e00b2a9107570488d8dcc
Lars Hartvigsen <lars.hartvigsen@nav.no>
2026-04-22 09:04:43 +0200

    TOB-6519 Endringer etter qa

--- src/pages/sokos-skattekort-admin/[...proxy].ts
+++
@@ -1,8 +0,0 @@
-import { routeProxyWithOboToken } from "@utils/server/proxy";
-import type { APIRoute } from "astro";
-
-export const ALL: APIRoute = routeProxyWithOboToken({
-	apiProxy: `${process.env.SOKOS_SKATTEKORT_API_PROXY}`,
-	apiUrl: `${process.env.SOKOS_SKATTEKORT_API}`,
-	audience: `${process.env.SOKOS_SKATTEKORT_API_AUDIENCE}`,
-});
