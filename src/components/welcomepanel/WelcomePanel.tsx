a869f3e32a956fff94abb75eecd8869689855221
mudasar187 <mudasar_cheema@hotmail.com>
2026-08-17 10:55:39 +0200

    send inn slot istedenfor props basert på astro best praksis

--- src/components/welcomepanel/WelcomePanel.tsx
+++ src/components/welcomepanel/WelcomePanel.tsx
@@ -1,13 +1,19 @@
-import { BodyLong, GuidePanel, Heading, Link } from "@navikt/ds-react";
+import { GuidePanel, Heading } from "@navikt/ds-react";
+import type { ReactNode } from "react";
 import moneyBag from "../../../public/images/pengesekk.svg";
 import styles from "./WelcomePanel.module.css";
 
 type WelcomePanelProps = {
 	name: string;
 	greeting: string;
+	children: ReactNode;
 };
 
-export default function WelcomePanel({ name, greeting }: WelcomePanelProps) {
+export default function WelcomePanel({
+	name,
+	greeting,
+	children,
+}: WelcomePanelProps) {
 	return (
 		<div className={styles.welcomeGuidepanel}>
 			<div className={styles.welcomeGuidepanel__heading}>
@@ -17,25 +23,7 @@ export default function WelcomePanel({ name, greeting }: WelcomePanelProps) {
 			</div>
 			<div className={styles.welcomeGuidepanel__panel}>
 				<GuidePanel illustration={<img src={moneyBag.src} alt="" />}>
-					<Heading level="2" size="small" spacing>
-						Ny design!
-					</Heading>
-					<BodyLong spacing>
-						Fordi Aksel, designspråket til Nav, har oppdatert til versjon 8 har
-						vi måttet gjøre noen endringer og dermed også forbedringer. Du vil i
-						en overgangsperiode oppleve at applikasjonene endrer over til nytt
-						design i forskjellig tempo og det vil være litt forskjeller.
-					</BodyLong>
-					<BodyLong>
-						<Link
-							href="https://navno.sharepoint.com/sites/fag-og-ytelser-stonadsokonomi/SitePages/Utbetalingsportalen.aspx"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Les mer om Utbetalingsportalen og hvordan du får tilgang til de
-							ulike arbeidsflatene (Navet)
-						</Link>
-					</BodyLong>
+					{children}
 				</GuidePanel>
 			</div>
 		</div>
