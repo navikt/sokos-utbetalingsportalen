eb1a264c8eedf11d61ed65c918f395fbdf7c8ae9
mudasar187 <mudasar.cheema@nav.no>
2026-09-25 23:19:32 +0200

    TOB-7069: sletter spk mottak dashoard fra utbetalingsportalen, legger til som extern url i aksel v8 branch

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -68,15 +68,6 @@ export const apps: App[] = [
 		naisAppName: "sokos-up-skattekort-admin",
 	},
 	{
-		app: "SPK-MOTTAK",
-		title: "SPK mottak",
-		description: "Dashboard for å trigge jobber",
-		adGroupDevelopment: "0e58dc41-7c57-4b79-a8c7-d0caec129e53",
-		adGroupProduction: "f49b797d-600d-46da-8061-7746e48d1aca",
-		route: "/spk-mottak",
-		naisAppName: "sokos-up-spk-mottak",
-	},
-	{
 		app: "RESENDING-BANK",
 		title: "Resending til bank",
 		description: "Resending av meldinger til bank",
